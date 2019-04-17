757c66978846586da24658d1e7daf3e80f0d53c3
Esmfan <zbigniew.kaczynski@op.pl>
2019-04-17 11:15:00 +0900

    Final version

--- TicTacToe/Assets/Scripts/Game.cs
+++ TicTacToe/Assets/Scripts/Game.cs
@@ -1,82 +1,157 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
-    private Board mainBoard;
-   // private Vector2Int bestMove;
 
+    private SimpleBoard mainBoard;
     public int width;
     public int height;
-
     public Squere squerePrefab;
     Squere[,] squeres;
     private float interval = 2.0f;
     public Sprite empty;
     public Sprite x;
     public Sprite o;
+    public Image currentPlayerImage;
+    public Text currentPlayerText;
+    public Button XSetter;
+    public Text XSetterText;
+    private bool xIsAi;
+    private bool oIsAi;
 
-    private struct scoreAndMove{
-        public Vector2Int move;
+    private struct ScoreAndMove {
+        public int move;
         public int score;
-
-        public scoreAndMove(int s,Vector2Int vec ) {
-            move = vec;
+        public ScoreAndMove(int s, int m) {
+            move = m;
             score = s;
         }
-
     }
-
-   
-
-
     void Start() {
-
         squeres = new Squere[width,height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 CreateTile(x, y);
             }
         }
-
-        mainBoard = new Board(width, height,Board.Player.XPlayer);
-        /*
-        mainBoard.MakeMove(new Vector2Int(0, 0));
-        mainBoard.MakeMove(new Vector2Int(1, 1));
-        mainBoard.MakeMove(new Vector2Int(1,0));
-        mainBoard.MakeMove(new Vector2Int(2, 2));
-        mainBoard.MakeMove(new Vector2Int(2, 1));
-        */
-        Debug.Log(mainBoard.IsGameOver());
-        Debug.Log(mainBoard.Evaluate(Board.Player.XPlayer));
-        //scoreAndMove sam = Minimax(mainBoard, mainBoard.CurrentPlayer(), 9, 0);
-        scoreAndMove sam = Negamax(mainBoard, 9, 0);
-        Debug.Log(sam.move + "||" + sam.score);
-        /*
-        for (int i = 0; i < 5; i++) {
-            // Negamax(mainBoard, 10, 0);
-            scoreAndMove sam = Minimax(mainBoard, mainBoard.CurrentPlayer(), 9, 0);
-            Debug.Log("Best move"+sam.move);
-            
-
-            if (mainBoard.CurrentPlayer() == Board.Player.XPlayer) {
-                ChanheSprite(sam.move, o);
-            }
-            else {
-                ChanheSprite(sam.move, x);
-            }
-            mainBoard.MakeMove(sam.move);
-
-            // bestMove = new Vector2Int(-1,-1);
-
-
-            mainBoard.PrintBoard();
-
+        mainBoard = new SimpleBoard();
+        xIsAi = true;
+        oIsAi = true;
+        currentPlayerImage.GetComponent<Image>().sprite = x;
+    }
+    public void SetXAi() {
+        xIsAi = !xIsAi;
+        if (xIsAi) {
+            XSetterText.text = "Change X to Human";
+        } else {
+            XSetterText.text = "Change X to AI";
         }
-        */
+    }
+    public void SetOAi() {
+        oIsAi = !oIsAi;
+    }
+
+    private void ExecuteMove(int move) {
+        if (mainBoard.IsEnd()) {
+            if (mainBoard.state == SimpleBoard.State.DRAW) {
+                currentPlayerText.text = "IT IS A DRAW!";
+            } else if (mainBoard.state == SimpleBoard.State.XWON) {
+                currentPlayerText.text = "X HAS WON!";
+            } else if (mainBoard.state == SimpleBoard.State.OWON) {
+                currentPlayerText.text = "O HAS WON!";
+            }
+        }
+
+        else if (mainBoard.turn && !xIsAi && mainBoard.IsMoveValid(move)) {
+            ChangeSprite(move, x);
+            mainBoard.MakeMove(move);
+            currentPlayerImage.GetComponent<Image>().sprite = o;
+        }
+
+
     }
 
 
+
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Keypad1)) {
+            ExecuteMove(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2)) {
+            ExecuteMove(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3)) {
+            ExecuteMove(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4)) {
+            ExecuteMove(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5)) {
+            ExecuteMove(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6)) {
+            ExecuteMove(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad7)) {
+            ExecuteMove(6);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad8)) {
+            ExecuteMove(7);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad9)) {
+            ExecuteMove(8);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&&
+            ((mainBoard.turn && xIsAi) || (!mainBoard.turn && oIsAi))) {
+            if (!mainBoard.IsEnd()) {
+
+                ScoreAndMove sam = Negamax(mainBoard, 0);
+
+                int move = sam.move;
+
+               
+                if (!mainBoard.turn) {
+                    ChangeSprite(move, o);
+                    currentPlayerImage.GetComponent<Image>().sprite = x;
+                } else {
+                    ChangeSprite(move, x);
+                    currentPlayerImage.GetComponent<Image>().sprite = o;
+                }
+                mainBoard.MakeMove(move);
+            } else {
+                if (mainBoard.state == SimpleBoard.State.DRAW) {
+                    currentPlayerText.text = "IT IS A DRAW!";
+                }
+                else if (mainBoard.state == SimpleBoard.State.XWON) {
+                    currentPlayerText.text = "X HAS WON!";
+                }
+                else if (mainBoard.state == SimpleBoard.State.OWON) {
+                    currentPlayerText.text = "O HAS WON!";
+                }
+            }
+        }
+    }
+
+    private ScoreAndMove Negamax(SimpleBoard simp, int currentDepth) {
+        if (simp.state == SimpleBoard.State.DRAW) return new ScoreAndMove(0, -1);
+        else if (simp.state == SimpleBoard.State.XWON|| simp.state == SimpleBoard.State.OWON) return new ScoreAndMove(10-currentDepth, -1);
+        ScoreAndMove worst = new ScoreAndMove(1000, -1);
+        foreach (int move in simp.GetMoves()) {
+            simp.MakeMove(move);
+            ScoreAndMove sam = Negamax(simp, currentDepth + 1);
+            simp.UnDoMove(move);
+            sam.score = -sam.score;
+            if (sam.score < worst.score) {
+                worst.score = sam.score;
+                worst.move = move;
+            }
+        }
+        return worst;
+    }
 
     void CreateTile(int x, int y) {
         Vector2 position;
@@ -87,107 +162,33 @@ public class Game : MonoBehaviour {
         squeres[x,y].transform.localPosition = position;
     }
 
-    void ChanheSprite(Vector2Int vec, Sprite s) {
-        Debug.Log(vec);
-        squeres[vec.x, vec.y].GetComponent<SpriteRenderer>().sprite = s;
+    void ChangeSprite(int num, Sprite s) {
+        if (num == 0) {
+            squeres[0, 0].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 1) {
+            squeres[1, 0].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 2) {
+            squeres[2, 0].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 3) {
+            squeres[0, 1].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 4) {
+            squeres[1, 1].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 5) {
+            squeres[2, 1].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 6) {
+            squeres[0, 2].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 7) {
+            squeres[1, 2].GetComponent<SpriteRenderer>().sprite = s;
+        }
+        if (num == 8) {
+            squeres[2, 2].GetComponent<SpriteRenderer>().sprite = s;
+        }
     }
-
-
-    private scoreAndMove Minimax(Board board, Board.Player player, int maxDepth,int currentDepth) {
-        if (board.IsGameOver() || currentDepth == maxDepth) {
-            Debug.Log("Bottom of recursion");
-            return new scoreAndMove(board.Evaluate(player), new Vector2Int(0, 0));
-        }
-       
-        int bestScore;
-        if (board.CurrentPlayer() == player) bestScore = -1000;
-        else bestScore = 1000;
-        Vector2Int bestMove= new Vector2Int(-1,-1);
-
-        int k = 0;
-        foreach (Vector2Int move in board.GetMoves()) {  
-           // Board newBoard =  //board.Copy();
-            Debug.Log("Inner minimax "+k+" "+currentDepth+" "+move);
-            k++;
-            board.MakeMove(move);
-            scoreAndMove sam;
-
-            sam = Minimax(board, player, maxDepth, currentDepth+1);
-            
-            if (board.CurrentPlayer() == player) {
-                if (sam.score > bestScore) {
-                    bestScore = sam.score;
-                    bestMove = sam.move;
-                }
-            }
-            else {
-                if (sam.score < bestScore) {
-                    bestScore = sam.score;
-                    bestMove = sam.move;
-                }
-            }
-            board.UndoMove(move);   
-        }
-        return new scoreAndMove(bestScore, bestMove);
-    }
-
-
-    private scoreAndMove Negamax(Board board, int maxDepth, int currentDepth) {
-        if (board.IsGameOver() || currentDepth == maxDepth) {
-            Debug.Log("Bottom");
-            return new scoreAndMove(board.Evaluate(), new Vector2Int(-1, -1));
-        }
-
-        scoreAndMove bestChoice = new scoreAndMove(-1000, new Vector2Int(-1, -1));
-
-        foreach(Vector2Int move in board.GetMoves()) {
-            board.MakeMove(move);
-            scoreAndMove sac = Negamax(board,maxDepth,currentDepth++);
-            board.UndoMove(move);
-            sac.score = -sac.score;
-            if (sac.score > bestChoice.score) {
-                bestChoice.score = sac.score;
-                bestChoice.move = sac.move;
-            }
-        }
-        return bestChoice;
-    }
-
-    /*
-    public Tile tilePrefab;
-    public GameObject focusPrefab;
-
-    GameObject focus;
-    Tile[] tiles;
-
-
-
-    
-            focus = Instantiate<GameObject>(focusPrefab);
-        focus.transform.SetParent(transform, false);
-        focus.transform.localPosition = new Vector2(0.0f, 0.0f);
-
-
-
-
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            focus.transform.localPosition = focus.transform.localPosition + new Vector3(-interval, 0.0f, 0.0f);
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            focus.transform.localPosition = focus.transform.localPosition + new Vector3(interval, 0.0f, 0.0f);
-
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            focus.transform.localPosition = focus.transform.localPosition + new Vector3(0.0f, interval, 0.0f);
-
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            focus.transform.localPosition = focus.transform.localPosition + new Vector3(0.0f, -interval, 0.0f);
-
-        }
-
-    }*/
 }
---
+++ TicTacToe/Assets/Scripts/SimpleBoard.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBoard 
+{
+    public enum Tile {
+        X,O,EMPTY
+    }
+    public enum State {
+        XWON, OWON, DRAW, UNDECIDED
+    }
+    public bool turn; 
+    public Tile[] TableBoard;
+    public State state;
+    public SimpleBoard() {
+        state = State.UNDECIDED;
+        turn = true;
+        TableBoard = new Tile[9];
+        for (int i = 0; i < 9; i++) {
+            TableBoard[i] = Tile.EMPTY;
+        }
+    }
+    public void Next() {
+        turn = !turn;
+    }
+    public bool IsEnd() {
+        return (state != State.UNDECIDED);
+    }
+    public void Evaluate() {
+        if (TableBoard[0] == Tile.X && TableBoard[1] == Tile.X && TableBoard[2] == Tile.X ||
+            TableBoard[3] == Tile.X && TableBoard[4] == Tile.X && TableBoard[5] == Tile.X ||
+            TableBoard[6] == Tile.X && TableBoard[7] == Tile.X && TableBoard[8] == Tile.X ||
+            TableBoard[0] == Tile.X && TableBoard[3] == Tile.X && TableBoard[6] == Tile.X ||
+            TableBoard[1] == Tile.X && TableBoard[4] == Tile.X && TableBoard[7] == Tile.X ||
+            TableBoard[2] == Tile.X && TableBoard[5] == Tile.X && TableBoard[8] == Tile.X ||
+            TableBoard[0] == Tile.X && TableBoard[4] == Tile.X && TableBoard[8] == Tile.X ||
+            TableBoard[2] == Tile.X && TableBoard[4] == Tile.X && TableBoard[6] == Tile.X
+            ) {
+            state = State.XWON;
+        } else if (TableBoard[0] == Tile.O && TableBoard[1] == Tile.O && TableBoard[2] == Tile.O ||
+                TableBoard[3] == Tile.O && TableBoard[4] == Tile.O && TableBoard[5] == Tile.O ||
+                TableBoard[6] == Tile.O && TableBoard[7] == Tile.O && TableBoard[8] == Tile.O ||
+                TableBoard[0] == Tile.O && TableBoard[3] == Tile.O && TableBoard[6] == Tile.O ||
+                TableBoard[1] == Tile.O && TableBoard[4] == Tile.O && TableBoard[7] == Tile.O ||
+                TableBoard[2] == Tile.O && TableBoard[5] == Tile.O && TableBoard[8] == Tile.O ||
+                TableBoard[0] == Tile.O && TableBoard[4] == Tile.O && TableBoard[8] == Tile.O ||
+                TableBoard[2] == Tile.O && TableBoard[4] == Tile.O && TableBoard[6] == Tile.O
+                ) {
+            state = State.OWON;
+        } else {
+            int count = 0;
+            for (int i = 0; i < 9; i++) {
+                if (TableBoard[i] != Tile.EMPTY) {
+                    count++;
+                }
+                if (count == 9) state = State.DRAW;
+                else state = State.UNDECIDED;
+            }
+        }
+    }
+    public void MakeMove(int move) {
+        if (turn) {
+            TableBoard[move] = Tile.X;
+        }
+        else {
+            TableBoard[move] = Tile.O;
+        }
+        Evaluate();
+        Next();
+    }
+
+    public void UnDoMove(int move) {
+        TableBoard[move] = Tile.EMPTY;
+        Evaluate();
+        Next();
+    }
+
+    public List<int> GetMoves() {
+        List<int> possibleMoves = new List<int>();
+        for (int i = 0; i < 9; i++) {
+            if (TableBoard[i] == Tile.EMPTY) {
+                possibleMoves.Add(i);
+            }
+        }
+        return possibleMoves;
+    }
+
+    public bool IsMoveValid (int move) {
+        return (TableBoard[move] == Tile.EMPTY);
+
+    }
+
+    public void show() {
+        int count = 0;
+        for (int i = 0; i < 9; i++) {
+            if (TableBoard[i] != Tile.EMPTY) {
+                 Debug.Log(TableBoard[i]);
+                count++;
+            }
+        }
+        Debug.Log(count);
+    }
+}
